74b54d13ecb309d57340bd290da5c8191a49823d
SinBin10 <bincode10@gmail.com>
2025-05-29 00:29:45 +0530

    working on the Login Page 👍

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,11 @@
+import { Login } from "./Pages/Login";
+
 const App = () => {
-  return <h1 className="text-5xl">I am a good boy</h1>;
+  return (
+    <div>
+      <Login />
+    </div>
+  );
 };
 
 export default App;
---
+++ client/src/Pages/Login.jsx
@@ -0,0 +1,14 @@
+export const Login = () => {
+  return (
+    <div>
+      <div className="w-[482px] h-[422px] bg-[#3B3B3B] rounded-4xl flex justify-center items-center">
+        <div className="flex flex-col">
+          <label className="text-white" htmlFor="">
+            Email
+          </label>
+          <input className="bg-[#959292]" type="text" />
+        </div>
+      </div>
+    </div>
+  );
+};
