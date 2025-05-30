0eee8867a3811769898a9711d5c2cb6067562b29
SinBin10 <bincode10@gmail.com>
2025-05-31 02:35:32 +0530

    did the useState thing

--- client/src/Pages/Login.jsx
+++ client/src/Pages/Login.jsx
@@ -1,6 +1,9 @@
 import { Link } from "react-router-dom";
+import { useState } from "react";
 
 export const Login = () => {
+  const [emailValue, setEmailValue] = useState("");
+  const [password, setPassword] = useState("");
   return (
     <div>
       <form className="w-[482px] h-[500px] bg-[#3B3B3B] rounded-4xl flex flex-col justify-center items-center gap-y-11">
@@ -10,9 +13,11 @@ export const Login = () => {
             Email
           </label>
           <input
+            value={emailValue}
             className="bg-[#959292] h-10 mt-2 rounded-xl"
             type="email"
             required
+            onChange={(e) => setEmailValue(e.target.value)}
           />
         </div>
         <div className="flex flex-col w-full px-12">
@@ -20,9 +25,11 @@ export const Login = () => {
             Password
           </label>
           <input
+            value={password}
             className="bg-[#959292] h-10 mt-2 rounded-xl"
             type="password"
             required
+            onChange={(e) => setPassword(e.target.value)}
           />
         </div>
         <div className="flex flex-col items-center">
