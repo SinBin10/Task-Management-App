7b0d9264a138dd6989e90dafb92b4588e89d96e7
SinBin10 <bincode10@gmail.com>
2025-06-03 07:19:14 +0530

    fixing some styling issues in the signup.jsx file

--- client/src/Pages/SignUp.jsx
+++ client/src/Pages/SignUp.jsx
@@ -1,19 +1,18 @@
-
-import {Link} from "react-router-dom"
+import { Link } from "react-router-dom";
 
 export const SignUp = () => {
   return (
     <main className="block m-auto">
-      
       <section className=" block m-auto w-[482px] h-max bg-[#3B3B3B] rounded-4xl justify-items-center p-6">
-        
-      
         <div className="flex flex-col w-full px-12 justify-center">
-          
           {/* Header */}
           <header className="text-center">
-            <h1 className="text-white text-2xl font-bold mb-4 m-auto p-0.5">Sign Up</h1>
-            <p className="m-auto font text-blue-400">Already have an account? <Link to ="/login">Login</Link> </p>
+            <h1 className="text-white text-2xl font-bold mb-4 m-auto p-0.5">
+              Sign Up
+            </h1>
+            <p className="m-auto font text-blue-400">
+              Already have an account? <Link to="/login">Login</Link>{" "}
+            </p>
           </header>
 
           {/* Form Section */}
@@ -45,7 +44,7 @@ export const SignUp = () => {
               />
             </div>
 
-              {/* The password div will have script to validate length, character and all that */}
+            {/* The password div will have script to validate length, character and all that */}
             <div className="mb-4">
               <input
                 type="password"
@@ -56,14 +55,11 @@ export const SignUp = () => {
             </div>
 
             {/* Terms and Conditions */}
-            <div className="text-white text-sm mb-4 flex items-start">
-              <input className="mt-1"
-              type= "checkbox" 
-              required
-              rounded-xl
-              />
+            <div className="text-white text-sm mb-4 flex items-start rounded-xl">
+              <input className="mt-1" type="checkbox" required />
               <p className="">
-                By pressing agree and continue below, I agree to (our app's name goes here) terms and conditions of service and privacy policy
+                By pressing agree and continue below, I agree to (our app's name
+                goes here) terms and conditions of service and privacy policy
               </p>
             </div>
 
@@ -71,15 +67,14 @@ export const SignUp = () => {
             <div className="text-center">
               <button
                 type="submit"
-                className=" bg-blue-700 text-white p-2 rounded m-auto font-bold" rounded-xl
+                className=" bg-blue-700 text-white p-2 m-auto font-bold rounded-xl"
               >
-                Sign Up 
+                Sign Up
               </button>
             </div>
           </form>
         </div>
-
       </section>
     </main>
   );
-}
+};
