2cddd0b8d6fe3ce35bfd588df49d641793d1b3c2
SinBin10 <bincode10@gmail.com>
2025-06-07 01:00:57 +0530

    made the login authentication logic

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -2,9 +2,25 @@ const User = require("../models/userModel.js");
 const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
 
-const loginReponse = (req, res) => {
+const loginReponse = async (req, res) => {
+  const { email, password } = req.body;
+
   try {
-    res.send({ msg: "send response from the controller" });
+    const existingUser = await User.findOne({ email });
+    if (!existingUser) return res.send({ msg: "Please sign up first..." });
+
+    const checkPassword = await bcrypt.compare(
+      password,
+      existingUser.confirmPassword
+    );
+
+    if (!checkPassword) return res.send({ msg: "wrong password" });
+
+    const token = jwt.sign(
+      { userId: existingUser._id },
+      process.env.SECRET_KEY
+    );
+    res.send({ msg: "user logged in successfully" }, token);
   } catch (error) {
     console.log("some error occured...");
   }
@@ -15,8 +31,9 @@ const signUpReponse = async (req, res) => {
     const { name, email, confirmPassword } = req.body;
 
     const existingUser = await User.findOne({ email });
-    console.log(name, email, confirmPassword);
-    if (existingUser) return res.send({ msg: "user already exists" });
+    console.log("existing user", existingUser);
+    if (existingUser)
+      return res.send({ msg: "user already exists please Login" });
 
     const hashedPassword = await bcrypt.hash(confirmPassword, 10);
 
@@ -32,7 +49,7 @@ const signUpReponse = async (req, res) => {
     const decodedToken = jwt.decode(token);
     console.log(decodedToken);
 
-    res.json({ msg: "user created succesfully....", token: token });
+    res.json({ msg: "user created succesfully....", token });
 
     // const createUser = async () => {
     //   await User.create({
