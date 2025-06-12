48f2330181bf7e4f9a804a8acf23c1f19f6f1a0d
SinBin10 <bincode10@gmail.com>
2025-06-12 22:43:04 +0530

    making protected routes (sending token from local storage to server to check validity)

--- client/src/Pages/Dashboard.jsx
+++ client/src/Pages/Dashboard.jsx
@@ -1,3 +1,16 @@
-export const Dashboard = () => {
-  return <h1 className="text-white text-5xl">Welcome to the dashboard</h1>;
+import { useState } from "react";
+import axios from "axios";
+
+export const Dashboard = async () => {
+  const [message, setMessage] = useState("yooooooooo");
+
+  try {
+    const token = localStorage.getItem("token");
+    const res = await axios.get("https://localhost:5000/dashboard", {
+      headers: { Authorization: `Bearer ${token}` },
+    });
+    console.log(res);
+  } catch (error) {}
+
+  return <h1 className="text-5xl text-white">{message}</h1>;
 };
