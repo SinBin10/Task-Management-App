71c1170ba60b4d2f18f611b9cfd245ead0b0629d
SinBin10 <bincode10@gmail.com>
2025-06-20 08:15:29 +0530

    fix: axios error on dashboard error

--- client/src/Pages/Dashboard.jsx
+++ client/src/Pages/Dashboard.jsx
@@ -2,19 +2,23 @@ import { useState } from "react";
 import axios from "axios";
 
 export const Dashboard = () => {
-  const [message, setMessage] = useState("yooooooooo");
+  const [message, setMessage] = useState("");
 
   const handleDashboard = async () => {
     try {
       const token = localStorage.getItem("token");
-      const res = await axios.get("https://localhost:5000/dashboard", {
+      const res = await axios.get("http://localhost:5000/dashboard", {
         headers: { Authorization: `Bearer ${token}` },
       });
       console.log(res);
+      setMessage(res.data.msg);
     } catch (error) {
+      console.log(error);
       console.log("something went wrong in displaying the dashboard");
     }
   };
+
+  handleDashboard();
 
   return <h1 className="text-5xl text-white">{message}</h1>;
 };
